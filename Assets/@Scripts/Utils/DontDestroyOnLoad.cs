387f404c824f12a3dbcd2b8b6af5904b101947a7
ickim <yainin0119@gmail.com>
2025-08-06 22:55:48 +0900

    feat) Sequence 스크립트 및 싱글톤 패턴 추가

---
+++ Assets/@Scripts/Utils/DontDestroyOnLoad.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace JJORY.Utils
+{
+    public class DontDestroyOnLoad : MonoBehaviour
+    {
+        #region LifeCycle
+        private void Start()
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+        #endregion
+    }
+}
---
+++ Assets/@Scripts/Utils/SingletonObject.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace JJORY.Utils
+{
+    public class SingletonObject<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        #region Variable
+        private static T instance;
+        public static bool isInitialized = false; // 중복 생성 방지 용도의 플래그변수
+        
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null && isInitialized == false)
+                {
+                    instance = FindFirstObjectByType<T>();
+                    isInitialized = true;
+                    
+                    if (instance == null)
+                    {
+                        Debug.Log($">>>>> {typeof(T).Name} 인스턴스를 찾을 수 없습니다.");
+                    }
+                }
+
+                return instance;
+            }
+        }
+        #endregion
+
+        #region LifeCycle
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this as T;
+                isInitialized = true;
+            }
+            else if (instance != this)
+            {
+                Debug.Log($">>>>> {typeof(T).Name} 인스턴스가 이미 존재하고 있습니다.");
+                Destroy(gameObject);
+            }
+        }
+        #endregion
+    }
+}
