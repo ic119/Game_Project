5f9f33c0b5098b9de0577cc8b3ec24c3c0ef7078
ickim <yainin0119@gmail.com>
2025-08-10 01:16:51 +0900

    feat) 프로그램 시작 전 Module담당 처리 Scene 및 클래스 구축

---
+++ Assets/@Scripts/Controller/Scene/DummySceneController.cs
@@ -0,0 +1,19 @@
+﻿using JJORY.Module;
+using JJORY.Util;
+using System.Collections;
+using UnityEngine;
+
+namespace JJORY.Scene.Dummy
+{
+    public class DummySceneController : MonoBehaviour
+    {
+        class AddressableLoad : Sequence
+        {
+            public IEnumerator Execute()
+            {
+                Utils.CreateLogError<DummySceneController>("1. AddressableLoad module");    
+                yield return null;
+            }
+        }
+    }
+}
