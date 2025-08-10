28da0b44dd640aa6829eed7500c74cb8634ad01e
ickim <yainin0119@gmail.com>
2025-08-10 23:27:47 +0900

    feat) SceneModel & SceneLoder 구축

---
+++ Assets/@Scripts/Controller/SceneLoadController.cs
@@ -0,0 +1,31 @@
+﻿using JJORY.Model;
+using JJORY.Util;
+using Photon.Pun.Demo.PunBasics;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JJORY.Module
+{
+    public class SceneLoadController : SingletonObject<SceneLoadController>
+    {
+        #region Variable
+        [SerializeField] private SceneModel cur_SceneModel;
+        private Dictionary<string, SceneModel> sceneModel_Dictionary = new Dictionary<string, SceneModel>();
+
+        [Range(0.0f, 2.0f)]
+        [SerializeField] private float delayTime;
+        [SerializeField] private string address = "Load_Scene";
+
+        public float cur_LoadProgress { get; private set; } = 0.0f;
+        public bool isLoading { get; private set; } = false;
+        #endregion
+
+        #region Method
+        public void Init(Action _onReady)
+        {
+            
+        }
+        #endregion
+    }
+}
