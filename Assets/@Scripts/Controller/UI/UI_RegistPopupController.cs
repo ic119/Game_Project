93de694a5a4f6b0bfbbe16f3a1842449d148ef3c
ickim <yainin0119@gmail.com>
2025-09-03 21:26:18 +0900

    feat) 회원가입 UI 구축

---
+++ Assets/@Scripts/Controller/UI/UI_RegistPopupController.cs
@@ -0,0 +1,37 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace JJORY.Controller.UI
+{
+    public class UI_RegistPopupController : MonoBehaviour
+    {
+        #region Variable
+        [Header("UI 변수")]
+        [SerializeField] private TMP_InputField accout_InputField;
+        [SerializeField] private TMP_InputField password_InputField;
+        [SerializeField] private Button cancel_Button;
+        [SerializeField] private Button craete_Button;
+        #endregion
+
+        #region LifeCycle
+        #endregion
+
+        #region Method
+        private void RegistInfoSave()
+        {
+
+        }
+
+        private void OnClickCancelButton()
+        {
+
+        }
+
+        private void OnClickCraeteButton()
+        {
+
+        }
+        #endregion
+    }
+}
