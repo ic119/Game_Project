6cfec8a74f560f6d7710f9978618c66e53809d16
ickim <yainin0119@gmail.com>
2025-09-03 19:25:16 +0900

    feat) LoginScene UI 생성 처리 추가

--- Assets/@Scripts/Controller/Scene/LoginScene.cs
+++ Assets/@Scripts/Controller/Scene/LoginScene.cs
@@ -1,5 +1,7 @@
 ﻿using JJORY.Module;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 
 namespace JJORY.Scene
@@ -17,11 +19,30 @@ namespace JJORY.Scene
 
         private void Start()
         {
-            //GameObject go = AddressableController.Instance.InstantiatePrefab<GameObject>("UI_LoginScene");
+            StartCoroutine(InstantiateAsset("UI_LoginScene"));
         }
         #endregion
 
         #region Method
+        private IEnumerator InstantiateAsset(string _key)
+        {
+            AsyncOperationHandle handler;
+
+            while (!AddressableController.Instance.GetHandler(_key, out handler))
+            {
+                yield return null;
+            }
+
+            // 로딩 완료될 때까지 대기
+            while (!handler.IsDone)
+            {
+                yield return null;
+            }
+
+            GameObject prefab = handler.Result as GameObject;
+            GameObject go = AddressableController.Instance.InstantiatePrefab(_key, prefab);
+            go.transform.parent = gameObject.transform;
+        }
         #endregion
     }
 }
