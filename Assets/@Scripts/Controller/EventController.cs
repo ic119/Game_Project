9524c5b5d74165883b0e32aad1558bea0fceaa68
ickim <yainin0119@gmail.com>
2025-09-21 21:28:29 +0900

    Add) Event 등록 및 구독 구축

---
+++ Assets/@Scripts/Controller/EventController.cs
@@ -0,0 +1,32 @@
+﻿using JJORY.Util;
+using System;
+using UnityEngine;
+
+namespace JJORY.Module
+{
+    public class EventController : SingletonObject<EventController>
+    {
+        #region Variable
+        public event Action<string, string> OnRequestShowPopup;
+        #endregion
+
+        #region LifeCycle
+        private void OnDestroy()
+        {
+            OnRequestShowPopup = null;
+        }
+        #endregion
+
+        #region Methoe_InvokeEvent
+        /// <summary>
+        /// 알람 팝업창 표출 이벤트
+        /// </summary>
+        /// <param name="_title"></param>
+        /// <param name="_content"></param>
+        public void InvokeShowPopup(string _title, string _content)
+        {
+            OnRequestShowPopup?.Invoke(_title, _content);   
+        }
+        #endregion
+    }
+}
