e4a667aa0978c7a6ad87400ac3cda78558ca6e6f
ickim <yainin0119@gmail.com>
2025-11-10 19:18:33 +0900

    Update) 캐릭터 생성 팝업창 추가

---
+++ Assets/@Scripts/Controller/UI/UI_GenerateCharacterPopupController.cs
@@ -0,0 +1,50 @@
+using JJORY.Module;
+using JJORY.Util;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace JJORY.Controller.UI
+{
+    public class UI_GenerateCharacterPopupController : MonoBehaviour
+    {
+        #region Variable
+        [Header("UI 변수")]
+        [SerializeField] private Button check_Button;
+        #endregion
+
+        #region LifeCycle
+        private void OnEnable()
+        {
+            if (check_Button != null)
+            {
+                check_Button.onClick.AddListener(OnClickedCheckButton);
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (check_Button != null)
+            {
+                check_Button.onClick.RemoveListener(OnClickedCheckButton);
+            }
+        }
+        #endregion
+
+        #region Method
+        private void OnClickedCheckButton()
+        {
+            if (GameManager.Instance != null)
+            {
+                if(GameManager.Instance.isUserData == false)
+                {
+
+                }
+            }
+            gameObject.SetActive(false);
+
+            Utils.CreateLogMessage<UI_GenerateCharacterPopupController>("캐릭터 생성 팝업 내 확인버튼 클릭");
+        }
+        #endregion
+    }
+}
