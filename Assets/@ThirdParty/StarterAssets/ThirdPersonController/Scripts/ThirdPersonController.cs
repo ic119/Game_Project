fc79703d850583447c078a646c595ab45f8304fa
ickim <yainin0119@gmail.com>
2025-12-12 19:04:51 +0900

    Update) Move 호출 시 MainCamera Null 에러 해결

--- Assets/@ThirdParty/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/@ThirdParty/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -104,7 +104,7 @@ namespace StarterAssets
         private Animator _animator;
         private CharacterController _controller;
         private StarterAssetsInputs _input;
-        private GameObject _mainCamera;
+        [SerializeField]private GameObject _mainCamera;
 
         private const float _threshold = 0.01f;
 
@@ -126,14 +126,16 @@ namespace StarterAssets
         private void Awake()
         {
             // get a reference to our main camera
-            if (_mainCamera == null)
-            {
-                _mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
-            }
+            //if (_mainCamera == null)
+            //{
+            //    _mainCamera = Camera.main.gameObject;
+            //    //_mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+            //}
         }
 
         private void Start()
         {
+            _mainCamera = Camera.main.gameObject;
             _cinemachineTargetYaw = CinemachineCameraTarget.transform.rotation.eulerAngles.y;
             
             _hasAnimator = TryGetComponent(out _animator);
