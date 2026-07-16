d5f1baf097539bfe92fd244d2be1b9d859fe2faa
ickim <yainin0119@gmail.com>
2026-07-16 17:54:58 +0900

    Update) 메인화면 옵션 메뉴 UI 구현

---
+++ Assets/@Scripts/View/UI/UI_MainSceneView.cs
@@ -0,0 +1,18 @@
+using Unity.AppUI.UI;
+using UnityEngine;
+
+public class UI_MainSceneView : MonoBehaviour
+{
+    #region Variable
+    [Header("UI 변수")]
+    [SerializeField] private GameObject mainPopup;
+    [SerializeField] private Button optionButton;
+    [SerializeField] private Button menuButton;
+    #endregion
+
+    #region LifeCycle
+    #endregion
+
+    #region Method
+    #endregion
+}
