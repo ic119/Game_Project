839acb6cb49179f1a35cebb83992591487538b6e
ickim <yainin0119@gmail.com>
2026-08-09 22:45:21 +0900

    Update) 폴더 구조 변경

---
+++ Assets/@Scripts/Presenter/Scene/LoginSceneManager.cs
@@ -0,0 +1,43 @@
+using Incheol.Models.Define;
+using Incheol.Modules;
+using Incheol.Utils;
+using UnityEngine;
+
+namespace Incheol.Presenter.Scene
+{
+    public class LoginSceneManager : MonoBehaviour
+    {
+        #region Variable
+        [Header("Init UI Variable")]
+        [SerializeField] private GameObject initImageObject;
+        #endregion
+
+        #region LifeCycle
+        private void Start()
+        {
+            CreateLoginUI();
+        }
+        #endregion
+
+        #region Method
+        private void CreateLoginUI()
+        {
+            if (AddressableAssetManager.Instance == null)
+            {
+                DebugLogManager.GenerateErrorMessage<LoginSceneManager>("AddressableAssetManager.Instance가 null입니다.");
+                return;
+            }
+
+            AddressableAssetManager.Instance.LoadPrefabAddress<GameObject>(AddressableAssetKey.UI_LoginScene.ToString(), prefab =>
+            {
+                if (this == null)
+                {
+                    return;
+                }
+
+                AddressableAssetManager.Instance.InstantiatePrefab(prefab);
+            });
+        }
+        #endregion
+    }
+}
