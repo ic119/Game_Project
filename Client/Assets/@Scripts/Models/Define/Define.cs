df7739a52a4acdd2ea03a5bb84ef8f6ee05ca0b4
ickim <ickim@deltax.ai>
2026-09-18 09:37:23 +0900

    Update) ThirdPartyAsset 정리

--- Client/Assets/@Scripts/Models/Define/Define.cs
+++ Client/Assets/@Scripts/Models/Define/Define.cs
@@ -17,14 +17,14 @@ namespace Incheol.Models.Define
         UI_GameScene = 9,
         Farm = 10,
         UI_InventorySlot = 11,
-        Camp001 = 12,
-        OneHandedAttack01,
-        SpearAttack01,
-        TwoHandedAttack01,
-        OneHandedHit01,
-        SpearHit01,
-        TwoHandedHit01,
-        Monster_RedBoar
+        Floor001 = 12,
+        OneHandedAttack01 = 13,
+        SpearAttack01 = 14,
+        TwoHandedAttack01 = 15,
+        OneHandedHit01 = 16,
+        SpearHit01 = 17,
+        TwoHandedHit01 = 18,
+        Monster_RedBoar = 19
     }
 }
 
